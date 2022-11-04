ff5856c1318e7b41e00a610aa23542cf3169a6ce
shhhQuiettt <krzysztof.skro@gmail.com>
2022-11-04 23:42:07 +0100

    Fix docker-compose again

--- findit/settings.py
+++ findit/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'findit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        "NAME": os.environ["MARIADB_DATABASE"],
+        "USER": os.environ["MARIADB_USER"],
+        "PASSWORD": os.environ["MARIADB_PASSWORD"],
+        "PORT": os.environ["MARIADB_PORT"],
+        "HOST": "db",
     }
 }
 

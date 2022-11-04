f8c78c0513e8fefdc787d31349cf95ccd2987e2b
Tomasz Wiśniewski <tomasz@wisniewski.app>
2022-11-04 23:45:53 +0100

    Add pagination

--- lostitems/views.py
+++ lostitems/views.py
@@ -1,11 +1,19 @@
 from rest_framework import generics
 from .models import LostItem, Answer, Guess
 from .serializers import LostItemSerializer, AnswerSerializer, GuessSerializer
+from rest_framework.pagination import PageNumberPagination
+
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 25
+    page_size_query_param = "page_size"
+    max_page_size = 1000
 
 
 class LostItemList(generics.ListCreateAPIView):
     queryset = LostItem.objects.all()
     serializer_class = LostItemSerializer
+    pagination_class = StandardResultsSetPagination
 
 
 class AnswerList(generics.ListCreateAPIView):
